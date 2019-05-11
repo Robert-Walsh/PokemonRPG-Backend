8602101b77569fee841c422162d8bd5cc7a0da1c
Robert Walsh <robert.walsh1411@gmail.com>
2019-05-11 15:05:57 +0100

    Basic request for a player using an id

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Controllers
+{
+    public static class PlayerController
+    {
+        [FunctionName("GetPlayer")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string id = req.Query["id"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            id = id ?? data?.id;
+
+            List<Player> players = new List<Player>(){
+                new Player(){Id = 1, Name = "Robert"}
+            };
+            if(id != null){
+                var player = players.FirstOrDefault(x => x.Id.ToString() == id);
+
+                if(player != null){
+                    return (ActionResult)new OkObjectResult($"Found player named {player.Name}");
+                }
+                else {
+                    return (ActionResult)new OkObjectResult($"No player found with Id {id}");
+                }
+            }
+
+            return new BadRequestObjectResult("Please pass an id on the query string or in the request body");
+        }
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Player {
+    public int Id {get; set;}
+    public int UserId {get; set;}
+    public string Name {get; set;}
+    public int Experience {get; set;}
+    public int Level {get; set;}
+    public int MaxHealth {get; set;}
+    public int CurrentHealth {get; set;}
+    public DateTime CreatedOn {get; set;}
+}
